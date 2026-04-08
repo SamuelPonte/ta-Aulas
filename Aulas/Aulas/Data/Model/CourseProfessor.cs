1f27748b67de5390ae3fb33f7ed4c8c49d5636a7
Samuel Ponte <samuel.ramosponte@gmail.com>
2026-04-08 12:53:05 +0100

    Criação da tabela Model

---
+++ Aulas/Aulas/Data/Model/CourseProfessor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aulas.Data.Model
+{
+    [PrimaryKey(nameof(CourseFK), nameof(ProfessorFK))]
+    public class CourseProfessor
+    {
+        /* *******************************
+         * Relacionamentos N-M regra 6
+         * Tabela de relacionamento N-M entre Course e Professor
+        ********************************** */
+        /// <summary>
+        /// FK para disciplina associada ao professor
+        /// </summary>
+        [ForeignKey(nameof(Course))]
+        [Display(Name = "Disciplina")]
+        public int CourseFK { get; set; }
+
+        /// <summary>
+        /// FK para disciplina associada ao professor
+        /// </summary>
+        [ValidateNever]
+        [Display(Name = "Disciplina")]
+        public Course Course { get; set; } = null!;
+
+        /// <summary>
+        /// FK pra Professor associado à disciplina
+        /// </summary>
+        [ForeignKey(nameof(Professor))]
+        [Display(Name = "Professor")]
+        public int ProfessorFK { get; set; }
+
+        /// <summary>
+        /// FK pra Professor associado à disciplina
+        /// </summary>
+        [ValidateNever]
+        [Display(Name = "Professor")]
+        public Professor Professor { get; set; } = null!;
+    }
+}
---
+++ Aulas/Aulas/Data/Model/Professor.cs
@@ -0,0 +1,13 @@
+﻿namespace Aulas.Data.Model
+{
+    public class Professor : MyUser
+    {
+        /* *******************************
+         * Relacionamentos N-M
+        ********************************** */
+        /// <summary>
+        /// Lista de disciplinas associadas ao professor, através da tabela de relacionamento N-M CourseProfessor
+        /// </summary>
+        public ICollection<CourseProfessor> CourseProfessors { get; set; } = new List<CourseProfessor>();
+    }
+}
